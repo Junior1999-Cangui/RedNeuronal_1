bf69bb4cead15bb4bac89de34d976e850ffdf575
Junior1999-Cangui <65523972+Junior1999-Cangui@users.noreply.github.com>
2020-05-29 22:47:47 -0500

    CAMBIOS 1

--- RedNeuronal1.java
+++ RedNeuronal1.java
@@ -29,19 +29,19 @@ public class RedNeuronal1 {
         System.out.println("                     TRABAJO EN CLASE \n");
         
         System.out.println("Ingrese el tema de su paper:");
-        String tema=objeto.nextLine (); 
+        String temaCOMPLETO=objeto.nextLine (); 
         
         System.out.println("¿Qué es una red neuronal?");
         String red=objeto.nextLine ();
         
         System.out.println("¿Cuáles son los elementos que forman una red neuronal?");
-        String elementos=objeto.nextLine ();
+        String elemento=objeto.nextLine ();
         
         System.out.println("¿Cómo utilizaron la red neuronal en el proyecto que usted ha seleccionado?");
         String utilizacion=objeto.nextLine ();
         
         System.out.println("¿Cómo utilizaría usted la red neuronal como mejoramiento al proyecto propuesto?");
-        String mejorar=objeto.nextLine ();
+        String mejoramiento=objeto.nextLine ();
         
         System.out.println("¿En qué consistió el entrenamiento utilizando una red neuronal, indique el algoritmo utilizado?");
         String entrenamiento=objeto.nextLine ();
